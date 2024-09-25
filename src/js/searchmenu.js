23b2c3e285ef5763734f35a1a994e6a05956742f
daniilsonufrijuks <do00492@rvt.lv>
2024-09-25 17:50:18 +0300

    menu

---
+++ src/js/searchmenu.js
@@ -0,0 +1,4 @@
+document.getElementById('navSearchBtn').addEventListener('click', function() {
+    const searchForm = document.getElementById('navSearchForm');
+    searchForm.classList.toggle('active');
+  });
